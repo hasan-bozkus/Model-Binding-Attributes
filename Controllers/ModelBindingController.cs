5fce0709ee001e0c4ffda34150f62e6dea6831e9
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-12-06 21:59:33 +0300

    Add project files.

---
+++ Controllers/ModelBindingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Model_Binding_Attributes.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        public IActionResult BindRequiredExp(QueryParameters queryParameters)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(new[]
+                {
+                 queryParameters.X, queryParameters.Y
+             });
+            }
+
+            return BadRequest();
+        }
+
+        public IActionResult FromHeader([FromHeader] string accept)
+        {
+            return Ok(new[] { accept });
+        }
+
+        public class QueryParameters
+        {
+            [BindNever]
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+    }
+}
